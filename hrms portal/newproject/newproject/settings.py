df8b1f98d8d5db648ef166918931ee63acccb938
Lokesh <chowduvadal@gmail.com>
2026-05-28 13:10:51 +0530

    final backend deployment setup

--- hrms portal/newproject/newproject/settings.py
+++ hrms portal/newproject/newproject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+import dj_database_url
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-flaalqdcpopqo2x&p7)$h^ey6xvu#zq!*$*n7bkrmr)o_)5^=y'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.getenv("DEBUG") == "True"
 ALLOWED_HOSTS = ['*']
 
 
@@ -80,14 +82,7 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hrm_db',
-        'USER': 'postgres',
-        'PASSWORD': 'lokesh@11',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
 
 # Password validation
